048d27d38b5df014a2e7ae5e145384d68f1eaf17
Miftah Salam <salam.miftah@gmail.com>
2023-08-10 08:26:40 +0700

    update user service

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UserDTO } from '../model/dto/user-dto';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BaseResponse } from '../model/response/base-response';
 
 @Injectable({
@@ -10,6 +10,7 @@ import { BaseResponse } from '../model/response/base-response';
 })
 export class UserService {
   baseUrl = environment.baseUrl
+  baseUserUrl = this.baseUrl + '/users'
   dataChange: BehaviorSubject<UserDTO[]> = new BehaviorSubject<UserDTO[]>([])
   dataChangeUser: BehaviorSubject<UserDTO | null> = new BehaviorSubject<UserDTO | null>(null)
   dataChangeImage: BehaviorSubject<any> = new BehaviorSubject<any>(null)
@@ -17,16 +18,18 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public getUsers(): Observable<UserDTO[]> {
-    this.httpClient.get(this.baseUrl+'/users/all').subscribe({next: (resp) => {
-      const baseResponse = resp as BaseResponse<UserDTO[]>
+    this.httpClient.get(this.baseUserUrl + '/all').subscribe({
+      next: (resp) => {
+        const baseResponse = resp as BaseResponse<UserDTO[]>
 
-      this.dataChange.next(baseResponse.data || [])
-    }, error: (err)  =>{
-      console.error("UserService-getUsers error: "+err)
-    }
-    , complete: () => {
-      console.info("UserService-getUsers complete")
-    }})
+        this.dataChange.next(baseResponse.data || [])
+      }, error: (err) => {
+        console.error("UserService-getUsers error: " + err)
+      }
+      , complete: () => {
+        console.info("UserService-getUsers complete")
+      }
+    })
 
     return this.dataChange.asObservable();
   }
@@ -35,14 +38,69 @@ export class UserService {
    * getUser
    */
   public getUser(email: string): Observable<UserDTO | null> {
-    this.httpClient.get(this.baseUrl+"/users/email/"+email).subscribe({
+    this.httpClient.get(this.baseUserUrl + "/email/" + email).subscribe({
       next: (resp) => {
         const baseResponse = resp as BaseResponse<UserDTO>
 
         this.dataChangeUser.next(baseResponse.data || null)
-        }
+      }
     })
 
     return this.dataChangeUser.asObservable()
+  }
+
+  /**
+   * updateUserEmail
+   */
+  public updateUserEmail(id: number, email: string) {
+    return this.httpClient.put(this.baseUserUrl + `/${id}`, email)
+  }
+
+  /**
+   * updateUserImage
+   */
+  public updateUserImage(id: number, imagePath: string) {
+    return this.httpClient.put(this.baseUserUrl + `/${id}/image`, imagePath)
+  }
+
+  /**
+   * updatePassword
+   */
+  public updatePassword(password: string, token: string) {
+    return this.httpClient.put(this.baseUserUrl + `/updatePassword/${token}`, password)
+  }
+
+  /**
+   * addImage
+   */
+  public addImage(image: FormData) {
+    this.httpClient.post(this.baseUrl + '/files/AVATAR', image).subscribe({
+      next: (resp) => {
+        const baseResponse = resp as BaseResponse<string>
+
+        this.dataChangeImage.next(baseResponse.data)
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.name + " " + err.message);
+      }
+    })
+
+    return this.dataChangeImage.asObservable()
+  }
+
+  /**
+   * getImage
+   */
+  public getImage(imagePath: string) {
+    this.httpClient.get(this.baseUrl + '/files', { params: { "imagePath": imagePath } }).subscribe({
+      next: (resp) => {
+        const baseResponse = resp as BaseResponse<string>
+
+        this.dataChangeImage.next(baseResponse.data)
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.name + " " + err.message);
+      }
+    })
   }
 }
