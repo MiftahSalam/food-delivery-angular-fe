fe62186400a0b9eaffa12dc32450d9b364e8220d
Miftah Salam <salam.miftah@gmail.com>
2023-08-10 07:28:40 +0700

    daily menu service

--- src/app/core/model/daily-menu.ts
+++ src/app/core/model/daily-menu.ts
@@ -2,7 +2,7 @@ import { Meal } from "./meal";
 import { WeeklyMenu } from "./weekly-menu";
 
 export interface DailyMenu {
-    id: number;
+    idailyMenuID: number;
     date: Date;
     meals: Meal[];
     weeklyMenu: WeeklyMenu;
