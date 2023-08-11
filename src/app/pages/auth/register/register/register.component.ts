7aeb98aa70ed3e2c86208b545985dfdc5402109a
Miftah Salam <salam.miftah@gmail.com>
2023-08-11 12:44:59 +0700

    register component

--- src/app/pages/auth/register/register/register.component.ts
+++ src/app/pages/auth/register/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserRegDTO } from 'src/app/core/model/dto/user-reg-dto';
+import { AuthService } from 'src/app/core/service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup = {} as FormGroup
+  errorMessage: string = ""
+  registerSuccess: boolean = false
+  success: string = ""
+  user: UserRegDTO = {} as UserRegDTO
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastrService: ToastrService
+  ) {
+
+  }
 }
