c8cb8206620b378c9fc88840686cc72b59eda427
Miftah Salam <salam.miftah@gmail.com>
2023-08-19 10:10:38 +0700

    table types page

--- src/app/pages/meal/meal.module.ts
+++ src/app/pages/meal/meal.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TypesComponent } from './types/types.component';
 import { MealComponent } from './meal/meal.component';
 import { MealRoutingModule } from './meal-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -13,6 +19,12 @@ import { MealRoutingModule } from './meal-routing.module';
   ],
   imports: [
     CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
     MealRoutingModule
   ]
 })
--- src/app/pages/meal/types/types.component.ts
+++ src/app/pages/meal/types/types.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { Type } from 'src/app/core/model/type';
+import { TypeService } from 'src/app/core/service/type.service';
 
 @Component({
   selector: 'app-types',
   templateUrl: './types.component.html',
   styleUrls: ['./types.component.css']
 })
-export class TypesComponent {
+export class TypesComponent implements OnInit {
+  types = [] as Type[]
+  show = false
+  currentType = {} as Type
+  displayedColumns = ['name', 'price', 'update']
+  dataSource = new MatTableDataSource<Type>
+  filterInput = {} as HTMLInputElement
+  @ViewChild(MatSort, { static: true }) typeSort = {} as MatSort
+  public searchString = ""
+  private successSub = new Subscription()
 
+  constructor(
+    private typeService: TypeService,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private toastrService: ToastrService
+  ) {
+    iconRegistry.addSvgIcon('search', sanitizer.bypassSecurityTrustResourceUrl('assets/search.svg'))
+  }
+
+  ngOnInit(): void {
+    this.successSub = this.typeService.updateSuccessEmitter.subscribe(data => {
+      this.getTypes()
+      this.toastrService.success("Successfully update a type", "Update type")
+    })
+
+    this.getTypes()
+
+    this.successSub = this.typeService.addSuccessEmitter.subscribe(data => {
+      this.getTypes()
+      this.toastrService.success("Successfully added new type", "Add type")
+    })
+  }
+
+  update(type: Type) {
+    if (type) {
+      this.show = true
+      this.currentType = type
+    }
+  }
+
+  applyFilter(filterValue: any) {
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
+
+  giveFocus() {
+    let filter = document.getElementById('filter')
+    filter?.focus()
+  }
+
+  getTypes() {
+    this.filterInput = document.getElementById('filterInput') as HTMLInputElement
+    this.filterInput.value = ""
+    this.typeService.getAllType().subscribe(data => {
+      if (data !== null && data.length > 0) {
+        this.types = data
+        this.dataSource = new MatTableDataSource(this.types)
+        this.dataSource.sort = this.typeSort
+      } else {
+        this.types = []
+      }
+    })
+  }
 }
